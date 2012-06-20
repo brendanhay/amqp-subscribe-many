62d0af457f457a8e78bc881c9e97cbf642cffb9d
brendanhay <brendan.g.hay@gmail.com>
2012-06-20 11:22:28 +0200

    Adding YAML configuration

--- script/run.rb
+++ script/run.rb
@@ -15,15 +15,17 @@ QUEUE     = "queue"
 KEY       = "key"
 
 EventMachine.run do
+  config = YAML::load(File.open(File.dirname(__FILE__) + "/config.yml"))
+
   # Consume
-  consumer = Messaging::Consumer.new([LOCALHOST, LOCALHOST])
+  consumer = Messaging::Consumer.new(config["consume_from"])
 
   consumer.subscribe(EXCHANGE, QUEUE, KEY) do |meta, payload|
     meta.ack
   end
 
   # Publish
-  producer = Messaging::Producer.new(LOCALHOST)
+  producer = Messaging::Producer.new(config["publish_to"])
 
   EventMachine::add_periodic_timer(1) do
     producer.publish(EXCHANGE, KEY, "some_random_payload")
