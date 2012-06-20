3933e4c62fa319f528baa7df3097f17019ecaa4a
brendanhay <brendan.g.hay@gmail.com>
2012-06-20 10:14:23 +0200

    Updating licensing

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -1,9 +1,3 @@
-# This Source Code Form is subject to the terms of
-# the Mozilla Public License, v. 2.0.
-# A copy of the MPL can be found in the LICENSE file or
-# you can obtain it at http://mozilla.org/MPL/2.0/.
-#
-
 require_relative "messaging/client"
 require_relative "messaging/producer"
 require_relative "messaging/consumer"
--- script/run.rb
+++ script/run.rb
@@ -1,11 +1,5 @@
 #!/usr/bin/env ruby
 
-# This Source Code Form is subject to the terms of
-# the Mozilla Public License, v. 2.0.
-# A copy of the MPL can be found in the LICENSE file or
-# you can obtain it at http://mozilla.org/MPL/2.0/.
-#
-
 require "rubygems"
 require "bundler"
 
@@ -22,17 +16,17 @@ KEY       = "key"
 
 EventMachine.run do
   # Consume
-  c = Messaging::Consumer.new([LOCALHOST, LOCALHOST])
+  consumer = Messaging::Consumer.new([LOCALHOST, LOCALHOST])
 
-  c.subscribe(EXCHANGE, QUEUE, KEY) do |meta, payload|
+  consumer.subscribe(EXCHANGE, QUEUE, KEY) do |meta, payload|
     meta.ack
   end
 
   # Publish
-  p = Messaging::Producer.new(LOCALHOST)
+  producer = Messaging::Producer.new(LOCALHOST)
 
   EventMachine::add_periodic_timer(1) do
-    p.publish(EXCHANGE, KEY, "some_random_payload")
+    producer.publish(EXCHANGE, KEY, "some_random_payload")
   end
 
   trap("INT") { EventMachine.stop }
